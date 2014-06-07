ad37c3970b5d766476b13f706dec75cf737dfd11
Seikun Kambashi <seikun_lee@hotmail.com>
2014-06-07 16:48:38 -0400

    heroku deployment test

--- server/app.js
+++ server/app.js
@@ -1,33 +1,16 @@
-// Load modules.
-var express = require('express'),
-    body_parser = require('body-parser'),
-    twilio = require('twilio'),
-    constants = require('./../../constants');
-
-// Start new express app.
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
-var port = 8000;
 
-// Create new REST API client to make authenticated requests against the twilio back end.
-var client = new twilio.RestClient(constants.twilio_sid, constants.auth_token);
+app.use(logfmt.requestLogger());
 
-// Pass in parameters to the REST API using an object literal notation.
-// The REST client will handle authentication and response serialization for you.
-client.sms.messages.create({
-  to: constants.to_phone,
-  from: constants.from_phone,
-  body:'Hello World! Testing twilio and node.js!'
-}, function(error, message){
-  // The HTTP request to Twilio will run asynchronously.
-  // This callback function will be called when a response
-  // from Twilio is received. The 'error' var will contain error info.
-  // If request is successful, 'error' value will be 'falsy'
-  if (!error) {
-    console.log('[RESPONSE] The SID for this SMS message is:');
-    console.log(message.sid);
-    console.log('[RESPONSE] Message sent on:');
-    console.log(message.dateCreated);
-  } else {
-    console.log('[ERROR] gg no re.')
-  }
+app.get('/', function(req, res){
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+
+app.listen(port, function(){
+  console.log("Listening on " + port);
 });
