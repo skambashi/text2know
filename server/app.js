148b448f628d67d2e9b4942fdf82c1727bfb44ab
Seikun Kambashi <seikun_lee@hotmail.com>
2014-06-08 17:35:26 -0400

    trying daemon

--- server/app.js
+++ server/app.js
@@ -1,65 +1,61 @@
-// To run node app in background of server
-// require('daemon')();
+// To run node app in background of server, daemonize.
+require('daemon')();
 
-// Initialize dependancies
+// Initialize dependancies.
 var express = require('express'),
-    // Use bunyan to log debug/error messages in JSON format
-    // log = require('bunyan').createLogger(
-    //   {
-    //     name: 'text2know',
-    //     streams: [{
-    //       level: 'debug',
-    //       path: '/home/ubuntu/text2know/server/logs/text2know.log'
-    //     },
-    //     {
-    //       level: 'error',
-    //       path: '/home/ubuntu/text2know/server/logs/text2know_error.log'
-    //     }]
-    //   }
-    // ),
+    Use bunyan to log debug/error messages in JSON format
+    log = require('bunyan').createLogger(
+      {
+        name: 'text2know',
+        streams: [{
+          level: 'debug',
+          path: '/home/ubuntu/text2know/server/logs/text2know.log'
+        },
+        {
+          level: 'error',
+          path: '/home/ubuntu/text2know/server/logs/text2know_error.log'
+        }]
+      }
+    ),
     hg = require('./views/html_gen'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
-// Create an express web app to run on port 80
+// Create an express web app to run on port 80.
 var app = express();
 var port = 80;
 
-// // Use middleware to parse incoming form bodies
+// Use middleware to parse incoming form bodies.
 app.use(bodyParser.urlencoded({extended : false}));
 
 app.post('/sms', function(request, response){
-  console.log('[SMS] POST request received:\n', request.body);
-  //log.debug('[SMS] Post request received:\n', request);
+  log.debug('[SMS] POST request received:\n', request);
 });
 
 /**************************** WWW ROUTES FROM HERE ***********************************/
 app.get('/', function(request, response) {
-  console.log('[WWW] GET request: \'/\'');
-  // log.debug('[WWW] GET request: \'/\'');
+  log.debug('[WWW] GET request: \'/\'');
   hg.genHtml(response, 'index.html');
 });
 
 /**************************** CSS/JS ROUTES FROM HERE ********************************/
 
 app.get('/static/css/:file', function(request, response) {
-  console.log('[CSS] GET request: \'/static/css/%s\'', request.params.file);
-  // log.debug('[CSS] GET request: \'/static/css/%s\'', request.params.file);
+  log.debug('[CSS] GET request: \'/static/css/%s\'', request.params.file);
   hg.genCss(response, request.params.file);
 });
 
 app.get('/static/js/:file', function(request, response) {
-  console.log('[JS] GET request: \'/static/js/%s\'', request.params.file);
-  // log.debug('[JS] GET request: \'/static/js/%s\'', request.params.file);
+  log.debug('[JS] GET request: \'/static/js/%s\'', request.params.file);
   hg.genJs(response, request.params.file);
 });
 
 app.get('/static/img/:file', function(request, response) {
-  console.log('[IMAGE] GET request: \'/static/img/%s\'', request.params.file);
-  // log.debug('[IMAGE] GET request: \'/static/img/%s\'', request.params.file);
+  log.debug('[IMAGE] GET request: \'/static/img/%s\'', request.params.file);
   hg.genImage(response, request.params.file);
 });
 
-console.log('[HTTP] Creating HTTP Server on Port', port.toString());
-//log.debug('[HTTP] Creating HTTP Server on Port ' + port.toString());
+/**************************** STARTING SERVER ****************************************/
+
+log.debug('[HTTP] Creating HTTP Server on Port ' + port.toString());
 app.listen(port);
