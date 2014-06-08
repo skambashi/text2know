75eedadada28f98575abcce4dd8e68d02215067a
Seikun Kambashi <seikun_lee@hotmail.com>
2014-06-07 22:50:08 -0400

    remove heroku tingz, hosting on ec2 instead

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res){
   res.send('Hello World!');
 });
 
-var port = Number(process.env.PORT || 5000);
+var port = Number(process.env.PORT || 8000);
 
 app.listen(port, function(){
   console.log("Listening on " + port);
