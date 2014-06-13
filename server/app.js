97115f4aa49eb6479f1dee0081d71098da7f9706
Seikun Kambashi <seikun_lee@hotmail.com>
2014-06-13 17:47:35 -0400

    test response text

--- server/app.js
+++ server/app.js
@@ -1,24 +1,25 @@
 // To run node app in background of server, daemonize.
-// require('daemon')();
+require('daemon')();
 
 // Initialize dependancies.
 var express = require('express'),
-    // Use bunyan to log debug/error messages in JSON format.
-    // log = require('bunyan').createLogger(
-    //   {
-    //     name: 'text2know',
-    //     streams: [{
-    //       level: 'debug',
-    //       path: '/home/ubuntu/text2know/server/logs/text2know.log'
-    //     },
-    //     {
-    //       level: 'error',
-    //       path: '/home/ubuntu/text2know/server/logs/text2know_error.log'
-    //     }]
-    //   }
-    // ),
+    Use bunyan to log debug/error messages in JSON format.
+    log = require('bunyan').createLogger(
+      {
+        name: 'text2know',
+        streams: [{
+          level: 'debug',
+          path: '/home/ubuntu/text2know/server/logs/text2know.log'
+        },
+        {
+          level: 'error',
+          path: '/home/ubuntu/text2know/server/logs/text2know_error.log'
+        }]
+      }
+    ),
     hg = require('./views/html_gen'),
     mongoose = require('mongoose'),
+    responder = require('./responder')
     bodyParser = require('body-parser');
 
 // Create an express web app to run on port 80.
@@ -28,40 +29,39 @@ var port = 80;
 // Use middleware to parse incoming form bodies.
 app.use(bodyParser.urlencoded({extended : false}));
 
-app.post('/sms', function(request, response){
-  console.log('[SMS] POST request received:\n', request.body);
-  // log.debug('[SMS] POST request received:\n', request.body);
-});
+app.post('/sms',
+  response.returnInput
+);
 
 /**************************** WWW ROUTES FROM HERE ***********************************/
 app.get('/', function(request, response) {
-  console.log('[WWW] GET request: \'/\'');
-  // log.debug('[WWW] GET request: \'/\'');
+  // console.log('[WWW] GET request: \'/\'');
+  log.debug('[WWW] GET request: \'/\'');
   hg.genHtml(response, 'index.html');
 });
 
 /**************************** CSS/JS ROUTES FROM HERE ********************************/
 
 app.get('/static/css/:file', function(request, response) {
-  console.log('[CSS] GET request: \'/static/css/%s\'', request.params.file);
-  // log.debug('[CSS] GET request: \'/static/css/%s\'', request.params.file);
+  // console.log('[CSS] GET request: \'/static/css/%s\'', request.params.file);
+  log.debug('[CSS] GET request: \'/static/css/%s\'', request.params.file);
   hg.genCss(response, request.params.file);
 });
 
 app.get('/static/js/:file', function(request, response) {
-  console.log('[JS] GET request: \'/static/js/%s\'', request.params.file);
-  // log.debug('[JS] GET request: \'/static/js/%s\'', request.params.file);
+  // console.log('[JS] GET request: \'/static/js/%s\'', request.params.file);
+  log.debug('[JS] GET request: \'/static/js/%s\'', request.params.file);
   hg.genJs(response, request.params.file);
 });
 
 app.get('/static/img/:file', function(request, response) {
-  console.log('[IMAGE] GET request: \'/static/img/%s\'', request.params.file);
-  // log.debug('[IMAGE] GET request: \'/static/img/%s\'', request.params.file);
+  // console.log('[IMAGE] GET request: \'/static/img/%s\'', request.params.file);
+  log.debug('[IMAGE] GET request: \'/static/img/%s\'', request.params.file);
   hg.genImage(response, request.params.file);
 });
 
 /**************************** STARTING SERVER ****************************************/
 
-console.log('[HTTP] Creating HTTP Server on Port ' + port.toString());
-// log.debug('[HTTP] Creating HTTP Server on Port ' + port.toString());
+// console.log('[HTTP] Creating HTTP Server on Port ' + port.toString());
+log.debug('[HTTP] Creating HTTP Server on Port ' + port.toString());
 app.listen(port);
---
+++ server/responder.js
@@ -0,0 +1,25 @@
+var twilio = require('twilio'),
+    log = require('bunyan').createLogger(
+      {
+        name: 'text2pay',
+        streams: [{
+          level: 'debug',
+          path: '/home/ubuntu/text2know/server/logs/text2know.log'
+        },
+        {
+          level: 'error',
+          path: '/home/ubuntu/text2know/server/logs/text2know_error.log'
+        }]
+      }
+    ),
+    constants = require('./../../constants')
+    client = require('twilio')(constants.twilio_sid, constants.auth_token);
+
+exports.returnInput = function(request, response){
+  // var tokens = request.body.Body.split(" ");
+  // console.log('[TEXT] Inbound SMS detected!');
+  log.debug('[TEXT] Inbound SMS detected!');
+  var twiml = new twilio.TwimlResponse();
+  twiml.message('Server received your message successfully: ' + request.body.Body);
+  response.send(twiml.toString());
+}
