e368f60a26a79555594e134ed5cc0edfc2c34745
Seikun Kambashi <seikun_lee@hotmail.com>
2014-06-14 14:14:35 -0400

    google start

--- server/responder.js
+++ server/responder.js
@@ -14,17 +14,23 @@ var twilio = require('twilio'),
     // ),
     constants = require('./../../constants'),
     client = require('twilio')(constants.twilio_sid, constants.auth_token),
+    reddit = require('./apps/google'),
     colors = require('colors');
 
 exports.help = function(request, response, next){
+  console.log('[TEXT] SMS detected:'.green, request.body.Body.green);
+  console.log('[TEXT] From:'.green,request.body.From.green);
+  console.log('[TEXT] Body:'.green,request.body.Body.green);
+
+  console.log('[TEXT] \'help\''.green);
+
   var tokens = request.body.Body.split(" ");
-  console.log('[TEXT] \'cmds\' detected:'.green, request.body.Body.green);
-  // log.debug('[TEXT] \'cmds\' detected:', request.body.Body);
+
   if (tokens[0].toLowerCase() == 'cmds'){
     var body = 'Commands:\n'
         + 'cmds | Returns a list of available commands.\n'
         + 'TODO | Add more commands.';
-    console.log('[DEBUG] TEXT FROM:',request.body.From);
+
     client.messages.create({
       to: request.body.From,
       from: constants.from_phone,
@@ -45,8 +51,13 @@ exports.help = function(request, response, next){
   }
 }
 
+exports.google = function(request, response, next){
+  console.log('[TEXT] \'google\''.green);
+
+}
+
 exports.invalid = function(request, response){
-  console.log('[TEXT] \'invalid\' detected:'.red, request.body.Body.red);
+  console.log('[TEXT] \'invalid\'');
   var body = 'Invalid input: ' + request.body.Body + '.\nPlease type \'cmds\' to get list of available commands.'
   client.messages.create({
     to: request.body.From,
